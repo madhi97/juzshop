f64770ebb47824fa59a2733145eff4c3cc15c43f
madhi.s <madhi.s@gofrugal.com>
2020-08-26 12:42:28 +0530

    Added tests for get and deleteb functions

--- src/test/java/com/ecommerce/juzshop/CartDaoImpltests.java
+++ src/test/java/com/ecommerce/juzshop/CartDaoImpltests.java
@@ -2,7 +2,7 @@ package com.ecommerce.juzshop;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 import com.ecommerce.juzshop.dao.CartDaoImpl;
 import com.ecommerce.juzshop.model.CartModel;
@@ -22,8 +22,10 @@ public class CartDaoImpltests {
 		ctx.register(AppConfig.class);
 		ctx.refresh();
 
-		Map<Integer, Integer> productlist = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> productlist = new HashMap<Integer, Integer>();
 		productlist.put(1, 1);
+		productlist.put(2, 3);
+		productlist.put(5, 7);
 
 		CartModel cart = new CartModel(4, 3, 1, "P", productlist);
 		CartDaoImpl cartinsert = new CartDaoImpl();
@@ -32,6 +34,12 @@ public class CartDaoImpltests {
 		cartinsert.insertcart(cart);
 		List<CartModel> cartlist = cartinsert.getallcartsummary();
 		System.out.println(cartlist.toString());
+		System.out.println(cartinsert.getcartsummarybyid(1).toString());
+		System.out.println(cartinsert.getcartitems(1).toString());
+		System.out.println(cartinsert.getcartsummarybyuser(1));
+		cartinsert.deletecartitem(8, 1);
+		cartinsert.deletecart(12);
+		System.out.println("!!!!!!!!!!!!!!Tests  Passed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		ctx.close();
 	}
 
