5bb25ceea744d3c32336f123fd175138282a6dbe
madhi.s <madhi.s@gofrugal.com>
2020-08-26 19:01:29 +0530

    Added Service class for cart  with getbyId

---
+++ src/main/java/com/ecommerce/juzshop/service/CartService.java
@@ -0,0 +1,37 @@
+package com.ecommerce.juzshop.service;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+import com.ecommerce.juzshop.dao.CartDaoImpl;
+import com.ecommerce.juzshop.model.CartModel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+    @Autowired
+    CartDaoImpl cartdao;
+
+    public CartModel getbyId(int cart_id) {
+
+        CartModel cart;
+
+        HashMap<Integer, Integer> product_list = new HashMap<Integer, Integer>();
+        cart = cartdao.getcartsummarybyid(cart_id);
+        for (Map temp : cartdao.getcartitems(cart_id)) {
+
+            product_list.put((Integer) temp.get("product_id"), (Integer) temp.get("quantity"));
+
+        }
+
+        cart.setProduct_list(product_list);
+
+        return cart;
+
+    }
+
+}
