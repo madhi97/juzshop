6eca5327d9a16ff4c4cbdff2596634b8c3a1d630
madhi.s <madhi.s@gofrugal.com>
2020-08-26 19:12:57 +0530

    Fixed the warning message in controller

--- src/main/java/com/ecommerce/juzshop/service/CartService.java
+++ src/main/java/com/ecommerce/juzshop/service/CartService.java
@@ -22,7 +22,7 @@ public class CartService {
 
         HashMap<Integer, Integer> product_list = new HashMap<Integer, Integer>();
         cart = cartdao.getcartsummarybyid(cart_id);
-        for (Map temp : cartdao.getcartitems(cart_id)) {
+        for (Map<String,Object> temp : cartdao.getcartitems(cart_id)) {
 
             product_list.put((Integer) temp.get("product_id"), (Integer) temp.get("quantity"));
 
