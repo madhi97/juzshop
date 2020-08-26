144e9a7211f639ab74738ae5f130548a8044bf82
madhi.s <madhi.s@gofrugal.com>
2020-08-26 19:02:38 +0530

    Added Controller class for cart with getbyId endpoint

---
+++ src/main/java/com/ecommerce/juzshop/controller/CartController.java
@@ -0,0 +1,26 @@
+package com.ecommerce.juzshop.controller;
+
+
+
+import com.ecommerce.juzshop.model.CartModel;
+import com.ecommerce.juzshop.service.CartService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CartController {
+    @Autowired
+    CartService cartService;
+
+    @GetMapping("/cart/getbyId/{cart_id}")
+    public CartModel cartgetbyId(@PathVariable("cart_id")  int cart_id){
+
+        return cartService.getbyId(cart_id);
+
+    }
+
+
+}
