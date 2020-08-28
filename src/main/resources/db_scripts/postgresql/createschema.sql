75080501ff00eb31069572e35e2d99d4d01bc359
madhi.s <madhi.s@gofrugal.com>
2020-08-28 16:59:37 +0530

    removed the schema for model class

--- src/main/resources/db_scripts/postgresql/createschema.sql
+++ src/main/resources/db_scripts/postgresql/createschema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE juzshop;
 CREATE SCHEMA juzshop;
 create table IF NOT EXISTS juzshop.PRODUCTS (
-    product_id SERIAL PRIMARY KEY,
+    product_id serial primary key,
     product_name varchar,
     selling_price integer,
     MRP integer,
@@ -23,22 +23,7 @@ create table IF NOT EXISTS juzshop.cart_dtl(
     item_amount INTEGER,
     CONSTRAINT fk_cart FOREIGN KEY(cart_id) REFERENCES cart_hdr(cart_id) ON DELETE CASCADE
 );
-create table IF NOT EXISTS juzshop.order_hdr(
-    order_id serial primary key,
-    user_id INTEGER,
-    total_item INTEGER,
-    bill_amount INTEGER,
-    order_status varchar,
-    bill_disc INTEGER
-);
-create table IF NOT EXISTS juzshop.order_dtl(
-    order_id INTEGER,
-    product_id integer,
-    quantity integer,
-    item_amount INTEGER,
-    disc_perc INTEGER,
-    CONSTRAINT fk_order FOREIGN KEY(order_id) REFERENCES order_hdr(order_id) ON DELETE CASCADE
-);
+
 create table IF NOT EXISTS juzshop.customer (
     cust_user_id SERIAL PRIMARY KEY,
     cust_user_name varchar,
