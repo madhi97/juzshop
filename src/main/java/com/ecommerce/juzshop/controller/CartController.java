8248d0c169dffbd9f82f6e8665d08ddb26c86744
madhi.s <madhi.s@gofrugal.com>
2020-08-31 19:17:27 +0530

    Added preauthorize to all endpoints and added required authorizations for the roles

--- src/main/java/com/ecommerce/juzshop/controller/CartController.java
+++ src/main/java/com/ecommerce/juzshop/controller/CartController.java
@@ -6,6 +6,7 @@ import com.ecommerce.juzshop.model.CartModel;
 import com.ecommerce.juzshop.service.CartService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ public class CartController {
     @Autowired
     CartService cartService;
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_EMPLOYEE')")
     @GetMapping("/cart/getbyId/{cart_id}")
     public CartModel cartgetbyId(@PathVariable("cart_id") int cart_id) {
 
@@ -25,40 +27,41 @@ public class CartController {
 
     }
 
+    @PreAuthorize("hasRole('ROLE_EMPLOYEE')")
     @GetMapping("/cart/getAll")
     public List<CartModel> cartgetall() {
         return cartService.getall();
     }
-
+   @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_EMPLOYEE')")
     @GetMapping("/cart/getbyuser/{user_id}")
     public List<CartModel> cartgetbyUser(@PathVariable("user_id") int user_id) {
 
         return cartService.getbyUser(user_id);
 
     }
-
+   @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_EMPLOYEE')")
     @PostMapping("/cart/insert")
     public void cartinsert(@RequestBody CartModel cart) {
         cartService.insertcart(cart);
 
     }
-
+   @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_EMPLOYEE')")
     @PostMapping("/cart/update")
     public void cartupdate(@RequestBody CartModel cart) {
         cartService.updatecart(cart);
     }
-
+   @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_EMPLOYEE')")
     @DeleteMapping("/cart/delete/{cart_id}")
     public void cartdelete(@PathVariable int cart_id) {
         cartService.deletecart(cart_id);
 
     }
-
+   @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_EMPLOYEE')")
     @DeleteMapping("/cart/delete/{cart_id}/{product_id}")
     public void removeitem(@PathVariable int cart_id, @PathVariable int product_id) {
         cartService.removeitems(cart_id, product_id);
     }
-
+   @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_EMPLOYEE')")
     @PostMapping("/cart/convertorder/{cart_id}")
     public void convertorder(@PathVariable int cart_id){
         cartService.convertcart(cart_id);
--- src/main/java/com/ecommerce/juzshop/controller/ProductController.java
+++ src/main/java/com/ecommerce/juzshop/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.ecommerce.juzshop.model.ProductModel;
 import com.ecommerce.juzshop.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,33 +18,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     @Autowired
     ProductService productService;
-
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_EMPLOYEE')")
     @GetMapping("/product/getAll")
     public List<ProductModel> getAllProduct() {
         return productService.getall();
     }
-
+   @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_EMPLOYEE')")
     @GetMapping("/product/getbyId/{product_id}")
     public ProductModel getproductbyId(@PathVariable int product_id) {
         return productService.getbyId(product_id);
     }
 
+    @PreAuthorize("hasRole('ROLE_EMPLOYEE')")
     @PostMapping("/product/insert")
     public void productinsert(@RequestBody ProductModel product) {
         productService.insertProduct(product);
 
     }
 
+    @PreAuthorize("hasRole('ROLE_EMPLOYEE')")
     @PostMapping("/product/update")
     public void productupdate(@RequestBody ProductModel product) {
         productService.updateProduct(product);
     }
 
+    @PreAuthorize("hasRole('ROLE_EMPLOYEE')")
     @PostMapping("/product/updatestock")
     public void productupdate(@RequestBody int product_id, @RequestBody int stock) {
         productService.updateProduct(product_id, stock);
     }
 
+    @PreAuthorize("hasRole('ROLE_EMPLOYEE')")
     @DeleteMapping("/product/delete/{product_id}")
     public void cartdelete(@PathVariable int product_id) {
         productService.deleteProduct(product_id);
