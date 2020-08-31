a011e343f1a70c31271e5363fb1bfe87bb32ccbd
madhi.s <madhi.s@gofrugal.com>
2020-08-31 19:15:08 +0530

    Added enableglobalwebsecurity with prepost enabled and permitted all requests to signup endpoint

--- src/main/java/com/ecommerce/juzshop/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/juzshop/WebSecurityConfig.java
@@ -4,12 +4,14 @@ import com.ecommerce.juzshop.securityfilter.JWTAuthorizationFilter;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 	@Configuration
 	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -19,6 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 				.authorizeRequests()
 				.antMatchers(HttpMethod.POST, "/login").permitAll()
+				.antMatchers(HttpMethod.POST, "/signup").permitAll()
 				.anyRequest().authenticated();
 		}
 	}
