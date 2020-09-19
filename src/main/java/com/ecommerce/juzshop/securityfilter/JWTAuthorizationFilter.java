6d16d520feea463689623e2b96b16aeb0973777c
madhi.s <madhi.s@gofrugal.com>
2020-09-20 00:27:10 +0530

    Sonar issue fix - made the constant sting as static

--- src/main/java/com/ecommerce/juzshop/securityfilter/JWTAuthorizationFilter.java
+++ src/main/java/com/ecommerce/juzshop/securityfilter/JWTAuthorizationFilter.java
@@ -21,9 +21,9 @@ import io.jsonwebtoken.UnsupportedJwtException;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
-	private final String HEADER = "Authorization";
-	private final String PREFIX = "Bearer ";
-	private final String SECRET = "oe4Dxfwp";
+	private static final String HEADER = "Authorization";
+	private static final String PREFIX = "Bearer ";
+	private static final String SECRET = "oe4Dxfwp";
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
@@ -66,7 +66,8 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
 	}
 
-	private boolean checkJWTToken(HttpServletRequest request, HttpServletResponse res) {
+	private boolean checkJWTToken(HttpServletRequest request, HttpServletResponse response) {
+		
 		String authenticationHeader = request.getHeader(HEADER);
 		if (authenticationHeader == null || !authenticationHeader.startsWith(PREFIX))
 			return false;
